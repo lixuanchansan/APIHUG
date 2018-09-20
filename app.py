b7e8e8286daad0816c8d7597ed3da4bed6edd03a
unknown <lixuan.lim.2016@sis.smu.edu.sg>
2018-09-20 11:30:56 +0800

    merge

--- app.py
+++ app.py
@@ -1,78 +1,12 @@
 #!/usr/bin/env python
-"""A simple API to do almost nothing"""
 import hug
-import gensim.downloader as api
-import os
 
-# To read files
-import codecs
-
-# To read filepaths
-import glob
-
-# Logging
-import logging
-
-# regular expression operators
-import re
-
-import nltk
-import pprint
-
-import gensim.models.word2vec as w2v
-
-# non-linear dimensional reduction
-import sklearn.manifold
-
-# set logging config
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-nltk.download("punkt")
-nltk.download("stopwords")
-nltk.download('wordnet')
-
-# Importing GloVe
-
-from gensim.scripts.glove2word2vec import glove2word2vec
-
-#glove_input_file = 'glove_50d.txt'
-#word2vec_output_file = 'word2vec.txt.word2vec'
-#glove2word2vec(glove_input_file, word2vec_output_file)
-
-from gensim.models import KeyedVectors
-from gensim.models.word2vec import Word2Vec
-
-# Loading the word2vec model
-#glove_w2v = KeyedVectors.load_word2vec_format(word2vec_output_file, binary = False)
-
-#i added this
-
-#corpus = api.load('text8')  # download the corpus and return it opened as an iterable
-#glove_w2v  = Word2Vec(corpus)
-
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
-
-stop_words = set(stopwords.words('english'))
-lemmatizer = WordNetLemmatizer()
-
-#file_names = glob.glob("data/insurance.txt")
-
-
-# Input: text
-# Output: Tree JSON
-@hug.local()
 @hug.get('/users', versions=1)
 def user(user_id):
     return 'I do nothing useful.'
     #return user_id
 
-@hug.post() 
-def getIntents(body):
-    """Returns the result of parsing through nlp engine"""
-    return body
-
-@hug.get() 
+@hug.get()
 def query_array(input: hug.types.text):
     """Assuming input to be an array of strings"""
 
