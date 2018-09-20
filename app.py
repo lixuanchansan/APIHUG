980c9042c199ca0ea12a7145edf6cf78c4475a1f
unknown <lixuan.lim.2016@sis.smu.edu.sg>
2018-09-20 11:46:12 +0800

    SW

--- app.py
+++ app.py
@@ -1,37 +1,20 @@
 #!/usr/bin/env python
 """A simple API to do almost nothing"""
 import hug
-
-users = [
-    {
-        'uid': u'jsmith',
-        'name': u'John Smith',
-        'email': u'jsmith@example.com',
-        'phone': u'123-456-7890'
-    },
-    {
-        'uid': u'jdoe',
-        'name': u'Jane Doe',
-        'email': u'jdoe@example.com',
-        'phone': u'234-567-8901'
-    },
-    {
-        'uid': u'ssample',
-        'name': u'Sally Sample',
-        'email': u'ssample@example.com',
-        'phone': u'345-678-9012'
-    }
-]
+@hug.default_input_format("application/json")
 
 @hug.post()
 def getIntents(body):
-    """Returns the result of parsing through nlp engine"""
-    return body
+  """  GETS TEXT IN JSON"""
+    parsed = hug.input_format.json(body) 
+    return parsed
 
 @hug.local()
 @hug.post()
-def treeify():
-    return {'users': users}
+def treeify(body):
+      """  GETS FLAT ARRAY IN JSON"""
+    parsed = hug.input_format.json(body) 
+    return parsed
 
 if __name__ == '__main__':
     user.interface.local()
